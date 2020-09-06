aa5099dd8552cad97c46ed74a26b246396a8597a
yves-cadour <yves.cadour@saint-louis29.net>
2020-09-06 18:05:04 +0200

    Ajout du fichier Fighter.py

---
+++ 2_Structures_de_données/Langage_Objet/Fighter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  6 17:55:17 2020
+
+@author: yves
+"""
+
+class Fighter:
+    """
+    La classe d'un fighter
+    """
+    pass # pour l'instant notre classe ne fait rien de spécial
+
+# Ensuite on va créer deux fighter marcel et maurice
+
+marcel=Fighter()
+maurice=Fighter()
