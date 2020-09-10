cafe9249806eefc87aa123608d6ccb3697fd05df
yves-cadour <yves.cadour@saint-louis29.net>
2020-09-10 10:38:28 +0200

    Ajout de fighterManager.py

---
+++ 2_Structures_de_données/Langage_Objet/fighterManager.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 10:32:55 2020
+
+@author: yves
+"""
+from fighter import Fighter
+
+class FighterManager:
+    """
+    Une classe qui va gérer les combats
+    """
+    pass
+
+rene=Fighter('rené')
